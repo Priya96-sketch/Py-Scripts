6841ca4df8e2c98062107a8b40316861aa406dbd
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-01-14 01:06:36 +0530

    Create shutdown_system.py

---
+++ shutdown_system.py
@@ -0,0 +1,10 @@
+---
+#Project - To Shutdown System in a given time
+#Author: Priya Mondal
+---
+
+#Module Used - pywhatkit
+import pywhatkit
+
+#Will shutdown the system in 100 seconds. Modify the time as per your choice.
+pywhatkit.shutdown(time=100)
